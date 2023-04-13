e1ff5253f904b6e751d9b83c8044f320312e7730
shelra90 <shelra90@yahoo.com>
2023-04-13 15:38:30 -0400

    changed font size and centered search box

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,20 +7,30 @@ import SearchBar from './SearchBar';
 const Header = () => {
   return (
     <header>
-        <Navbar bg="light" variant="light" expand="lg" collapseOnSelect>
+        <Navbar bg="light" variant="light" expand="lg" collapseOnSelect >
             <Container>
             <h1 className="title">Eventbrite</h1>
-            <SearchBar  placeholder="Search Events..."> </SearchBar>
+            <SearchBar  
+               
+              placeholder="Search Events...">
+                  
+              </SearchBar>
             <Button><i class="fa-solid fa-magnifying-glass"></i></Button>
+          
             <Navbar.Brand href="/"></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
+          
+            <Navbar.Toggle aria-controls="basic-navbar-na container-fluid"/>
+          
+            <Navbar.Collapse id="basic-navbar-nav container-fluid">
+           
                     <Nav className="ms-auto">
-                    <Nav.Link href="/cart"><i class="fa-sharp fa-solid fa-cart-shopping"></i> Cart</Nav.Link>  
-                    <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
-                    </Nav>
+                      
+                      <Nav.Link href="/cart"><i class="fa-sharp fa-solid fa-cart-shopping"></i> Cart</Nav.Link>  
+                      <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
+                      
+                      </Nav>
                 </Navbar.Collapse>
-            </Container>
+              </Container>
         </Navbar>
     </header>
   )
