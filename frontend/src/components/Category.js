9701b3fb535034bb00c21c6c8fe3b84d4a318844
shelra90 <shelra90@yahoo.com>
2023-04-15 14:48:17 -0400

    added category file and categoryscreen

---
+++ frontend/src/components/Category.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Card } from  'react-bootstrap'
+import { Button } from 'react-bootstrap'
+
+const Category = ({cat}) => {
+ return (
+    <div>
+      <Button>Music</Button>
+      <Button>Sports&Fitness</Button>
+      <Button>Food&Drink</Button>
+    </div>
+    )
+  }
+     export default Category
+  
