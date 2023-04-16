7c31db97a7efae787dbe6d85b445b667af2f800f
shelra90 <shelra90@yahoo.com>
2023-04-16 00:55:32 -0400

    created additional code for mapping  category button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,74 +1,53 @@
 import { Container } from "react-bootstrap";
+// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import Header from './components/Header';
 import Footer from "./components/Footer";
 import React from "react";
+// import events from "./events";
+// import {Link} from 'react-router-dom';
+
 
 
 
 function clickMe(){
   alert('You clicked me!');
 }
-// const Category = ({cat}) => ['Music', 'Sports&Fitness', 'Food&Drink']
-
-// // return (
-//   <div className='CategoryName'>
-//     {
-// button.Category.map((cat, index) => <div className='CategoryName'>key={}{cat}</div>)
-// }
-
-// </div>
-// // );
-
 
 function App () {
-  // const Category = ({cat}) => ['Music', 'Sports&Fitness', 'Food&Drink']
+//    const CategoryName = ({cat}) => ['Music', 'Sports&Fitness', 'Food&Drink']
+ 
   return ( 
       <>
+      {/* <Router> */}
       <Header />
       <main>
       <Container>
       <h3>Check Out These Trending Categories!</h3>
-    
+      <div className='CategoryName'>
+      {/* <Routes>
+      <Route path= "/" element={<HomeScreen />} exact/>
+      <Route path="/events/:CategoryName" element={events}/> */}
       <button class= "M" onClick={clickMe}>
             Music
       </button>
-    
+      {/* <Link to={`/event/CategoryName/${events._CategoryName}`}></Link> */}
       <button class= "S" onClick={clickMe}>
             Sports&Fitness
       </button>
-
       <button class="F" onClick={clickMe}>
             Food&Drink      
       </button>
-    
-
-</Container>
-        <HomeScreen />
-        </main>
-        <Footer />
+      {/* </Routes> */}
+      </div>
+      </Container>
+      <HomeScreen />
+      </main>
+      <Footer />
+      {/* </Router> */}
       </>
   );
 }
   
-
-//   return () {
-//       this.state = [
-//       { cat: "Music" },
-//       { cat: "Sports&Fitness" },
-//       { cat: "Food&Drink" },
-//   ];
-// }
-//   return (
-//     <div>
-//     {this.state.map((cat) = {
-//         return (
-//       <div>
-//        <h1>{cat.index}</h1>
-//       </div>
-//   );
-//   })}
-//  </div>
-// );
   
       export default App;
