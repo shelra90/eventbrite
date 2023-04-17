62317fe3431e69c21396d0dba8c29fcad9ddfd29
shelra90 <shelra90@yahoo.com>
2023-04-17 17:38:52 -0400

    added possible searchbar functionality

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,18 +1,60 @@
-import React from 'react';
-import { Form } from 'react-bootstrap'
+import React, { useState } from 'react';
+import events from '../events';
+// import { Form } from 'react-bootstrap'
+// var data = require('./events.js');
 
-const SearchBar = ({placeholder, data}) => {
+
+
+
+const SearchBar = () => {
+const [input, setInput] = useState("");
+
+  const fetchData = (value) => {
+    fetch(events).then((response) => response.js()).then(js => {
+    console.log(js);
+    }) ;
+  }
+
+  const handleChange = (value) => {
+    setInput(value)
+    fetchData(value) 
+  }
+
   return (
-    <div className="search">
-  <div className="searchInputs">
-      <div className="dataResult">
-        <Form.Control type="text" size="lg" placeholder={placeholder}> 
-        </Form.Control>
-           <div className="searchIcon">
-            </div>
-        </div>
-        </div>
-        </div>
+    <div className="input-wrapper">
+      <input placeholder="Type to search..." value={input} onChange={(e) => handleChange(e.target.value)} />
+    </div>
   )
-}
-export default SearchBar
+  }
+  export default SearchBar
+
+//   const[query, setQuery ] = useState("");
+//   const {events} = events;
+//   const {items} = events;
+
+//   const filteredItems = getFilteredItems = (query, items); 
+
+//   // const getFilteredItems = (query, items) => {
+//   //   if (!query) {
+//   //     return items;
+//   //   }
+//   //   return items.filter(events => event.name.includes(query))
+//   // }
+
+//   return (
+//     <div className="SearchBar">
+//       <label>Search</label>
+//       <input type="text" onChange={e => setQuery(e.target.value)} />
+//     </div>
+//   )
+// }
+
+
+
+
+
+
+
+
+
+ 
