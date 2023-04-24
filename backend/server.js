79dff1f96597fb2510c970f321bc18c3df4e1b05
shelra90 <shelra90@yahoo.com>
2023-04-23 20:55:51 -0400

    created config folder and created models folder

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express=require('express');
+import dotenv from 'dotenv'
 const events=require('./data/events');
+import connectDB from './config/db.js';
+
 const app=express();
+dotenv.config()
+connectDB()
 
 app.get('/api/events',(req,res)=>{
     res.json(events)
