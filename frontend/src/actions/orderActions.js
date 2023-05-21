6b963fe998746fddde65bc4177d266914bdd3990
shelra90 <shelra90@yahoo.com>
2023-05-21 18:19:08 -0400

    made final changes to placeorderscreen and orderaction

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -30,7 +30,8 @@ export const createOrder = (order) => async (dispatch,
                 },
             }
 
-            const { data } = await axios.post(`/api/orders`, order, config)
+            const { data } = await axios.post(`/api/orders`, order, 
+            config)
 
             dispatch({
                 type: ORDER_CREATE_SUCCESS,
@@ -71,7 +72,7 @@ export const createOrder = (order) => async (dispatch,
                     type: ORDER_DETAILS_SUCCESS,
                     payload: data,
                 })
-            }catch (error) {
+            } catch (error) {
                 dispatch({
                     type: ORDER_CREATE_FAIL,
                     payload: 
@@ -80,100 +81,45 @@ export const createOrder = (order) => async (dispatch,
                         : error.message
                 })
             }
+        }
+            export const payOrder = (orderId, paymentResult) => async (
+                dispatch,
+                getState
+            ) => {
+                try {
+                    dispatch({
+                        type: ORDER_PAY_REQUEST,
+                    })
+                const {
+                    userLogin: { userInfo },
+                } = getState()
 
-            return (
-                <>
-                <CheckoutSteps step1 step2 step3 step4 />
-                <Row>
-                    <Col md={8}>
-                        <ListGroup variant='flush'>
-                            <ListGroup.Item>
-                                <h2>Shipping</h2>
-                                <p>
-                                    <strong>Address:</strong>
-                                    {cart.shippingAddress.address}, {cart.
-                                    shippingAddress.city}{' '}
-                                    {cart.shippingAddress.postalCode}, {''}
-                                    {cart.shippinAddress.country}
-                                    </p>
-                            </ListGroup.Item>
+                const config ={
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${userInfo.token}`,
+                    },
+                }
+            
 
-                            <ListGroup.Item>
-                                <h2>Order Items</h2>
-                                {cart.cartItems.Length === 0 ? (
-                                    <Message> 
-                                        ) : (
-                                    <ListGroup variant='flush'>
-                                        {cart.cartItems.map((item, index) => (
-                                            <Row>
-                                                <Col md={1}>
-                                                    <Image
-                                                    src={item.image}
-                                                    alt={item.name}
-                                                    fluid
-                                                    rounded
-                                                    />
-                                                </Col>
-                                                <Col>
-                                                    <Link to ={`/event/${item.event}`}
-                                                    >
-                                                        {item.name}
-                                                    </Link>
-                                                    </Col>
-                                                    <Col md={4}>
-                                                {item.qty} x ${item.price} = ${item.
-                                                    qty * item.price}
-                                                    </Col>
-                                            </Row>
-                                            </ListGroup.Item>
-                                        ))}
-                                    </ListGroup>
-                                )}
-                     </ListGroup.Item>
-                     </ListGroup>
-                    </Col>
-                    <Col md={4}>
-                        <Card>
-                            <ListGroup.Item variant='flush'>
-                                <ListGroup.Item>
-                                    <h2>Order Summary</h2>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <Row>
-                                        <Col>Items</Col>
-                                        <Col>${cart.itemsPrice}</Col>
-                                    </Row>
-                                </ListGroup.Item>
-                            </ListGroup.Item>
-                            <Row>
-                                <Col>Total</Col>
-                                <Col>${cart.totalPrice}</Col>
-                            </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                            {error && <Message variant='danger'>{error}</Message>}
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Button
-                                type='button'
-                                className='btn-block'
-                                disabled={cart.cartItems === 0}
-                                onClick={placeOrderHandler}
-                                >
-                                    Place Order
-                                </Button>
-                            </ListGroup.Item>
-                            </ListGroup>
-                        </Card>
-                    </Col>
-                </Row>
-                </>
-        
-     }
+            const { data } = await axios.put(
+                `/api/orders${orderId}/pay`,
+                paymentResult,
+                config
+            )
 
-    export default PlaceOrderScreen
-
-
-
-
+            dispatch({
+                type: ORDER_PAY_SUCCESS,
+                payload: data,
+                })
+            } catch (error) {
+                dispatch({
+                    type: ORDER_PAY_FAIL,
+                    payload: 
+                        error.response && error.response.data.message
+                        ? error.response.data.message
+                        : error.message,
+                })
+            }
+        }
         
