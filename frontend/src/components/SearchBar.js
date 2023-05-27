d3af4de14cdcdcd4b5fd840e68238ea88a3803ab
shelra90 <shelra90@yahoo.com>
2023-05-27 18:22:38 -0400

    edits to searchbar

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,11 +1,46 @@
-/*  ignore */
+
 import React from 'react'
 import { Button } from 'react-bootstrap'
+// import axios from " axios "
+// import  { useState } from "react"
+
+// function Search() {
+
+// const [eventname, setName] = useState("");
+
+// async function serch(event) {
+//   event.preventDefault();
+
+//   try {
+//     axios.post("http://localhost:5467/events/" + eventname
+   
+//     ).then((res) => 
+//     {
+//       console.log(res.data.data.name);
+
+//       if (res.data.data =="User not found")
+//       {
+//         alert("User not found");
+
+//       }, fail => {
+//         console.error(fail); //Error
+//       }
+//     })
+//   }
+// }
+
 const SearchBar = ({placeholder, data}) => {
   return (
     <div className="search">
 
-        <input type="text" placeholder={placeholder} className="searchInputs"/>
+        <input type="text" 
+        placeholder={placeholder} 
+        className="searchInputs"
+        // value={eventname}
+        // // onChange={(event) => {
+        // //   // setName(event.target.value);
+        // }}
+        />
         <Button className='searchIcon'><i className="fa-solid fa-magnifying-glass"></i></Button>
         
     </div> 
@@ -14,5 +49,3 @@ const SearchBar = ({placeholder, data}) => {
 }
 
 export default SearchBar
-
-
